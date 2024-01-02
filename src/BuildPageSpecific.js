22a077c46bbca9465f7b678e3eaf15037d46909d
Nisarg Amin <nisarg.k.amin@gmail.com>
2024-01-02 10:29:20 -0500

    Input Element added for Build Specific page

--- src/BuildPageSpecific.js
+++ src/BuildPageSpecific.js
@@ -25,6 +25,42 @@ export const BuildPageSpecific = () => {
     <div className='kia-div' style={{marginTop: '40px'}}>
       <h1 style={{paddingLeft: '25px', fontSize: '40px'}}>2024 Forte</h1>
       <h3 style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between', paddingLeft: '25px', marginTop: '15px'}}><span style={{border: '1px solid black', borderRadius: '50%', fontSize: '30px', width: '40px', height: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', marginRight: '10px', fontWeight: '300'}}>1</span>Choose your Trim</h3>
+
+      <div>
+        <div className='trip-container'>
+          <button>
+            <div style={{display: 'flex', justifyContent: 'space-around', flexDirection: 'column', padding: '27px'}}>
+              <label for="baseTrim" style={{display: 'flex'}}>
+              <input id='baseTrim' type='radio' className='input-trip'/>
+              <div style={{display: 'flex', justifyContent: 'space-between', width: '100%'}}>
+              <p>Base Trim</p>
+              <p>$65.87/Weekly</p>
+              </div>
+              </label>
+              <ul className='listOfSpecification'>
+                <li>215 hp / 258 lb-ft Rear Wheel Drive</li>
+                <li>7-passenger seating</li>
+                <li>Battery Heating System</li>
+                <li>Fast Charge (400/800V)</li>
+                <li>Highway Driving Assist 2 (HDA2)</li>
+                <li>19” alloy wheels with aerodynamic covers</li>
+              </ul>
+            </div>
+          </button>
+          <button>
+            <div style={{display: 'flex', justifyContent: 'space-around' , padding: '50px'}}>
+              <p>Upgrade your ride</p>
+              <p>$4.79/Weekly</p>
+            </div>
+          </button>
+          <button>
+            <div style={{display: 'flex', justifyContent: 'space-around' , padding: '50px'}}>
+              <p>Upgrade your ride</p>
+              <p>$11.10/Weekly</p>
+            </div>
+          </button>
+        </div>
+      </div>
     </div>
     </div>
     </>
