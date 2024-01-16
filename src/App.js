b7cddbdfe41de8096b6de60b162767c7b27e7975
Nisarg Amin <nisarg.k.amin@gmail.com>
2024-01-16 12:20:51 -0500

    Mobile Version commit added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React, { useState, useEffect} from "react";
 import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Main from "./Main";
 import Header from "./Header";
@@ -8,10 +9,33 @@ import { SummaryPage } from "./SummaryPage";
 import { BookTestDrive } from "./BookTestDrive";
 import { BookingConfirmation } from "./BookingConfirmation";
 function App() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <Router>
-      <Header />
+      
+      {isMobile ? (
+        <div style={{display: "flex", justifyContent: "center", alignItems: "center", width: '100', height: '100vh',
+        marginTop: '0px',
+        textAlign: "center"
+        }}>
+          <h1>Mobile Version is not developed yet. Check in Desktop</h1>
+        </div>
+      ) : (
       <Routes>
+        <Header />
         <Route path="/BuildPage" element={<BuildPage />} />
         <Route path="/" element={<Main />} />
         <Route path="/BuildPage/BuildCar" element={<BuildPageSpecific />} />
@@ -19,6 +43,7 @@ function App() {
         <Route path="/book-test-drive" element={<BookTestDrive/>}/>
         <Route path="/BookingConfirmed" element={<BookingConfirmation/>}/>
       </Routes>
+      )}
     </Router>
   );
 }
